da098c0a04cc92c5776c7d50bf8c1bf1f5258b04
malikkurosaki <kurosakiblackangel@gmail.com>
2022-02-19 02:35:59 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,40 +3,40 @@ import { getDatabase, set, ref, child, onValue } from 'firebase/database';
 require('bootstrap/dist/js/bootstrap.bundle.min');
 const $ = require('jquery');
 
-let body = $(document.body).html('<div></div>');
+let body = $(document.body).html('<div>Server Ytb</div>');
 let conten = $('<div class="d-flex flex-row "></div>');
-let gambar = $(`<div class="col-md-6 col-sm-12 col-xs-12" id="gambar"></div>`);
-let log = $(`<div class="col-md-6 col-sm-12 col-xs-12" id="log"></div>`);
+let gambar = $(`<div class="col-6"></div>`);
+let log = $(`<div class="col-6"></div>`);
 
-body.append(conten);
-conten.append(gambar);
-conten.append(log);
+// body.append(conten);
+// conten.append(gambar);
+// conten.append(log);
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAfACNHRoyIvX4nct4juVabZDgwEDKQ6jY",
-    authDomain: "malikkurosaki1985.firebaseapp.com",
-    databaseURL: "https://malikkurosaki1985.firebaseio.com",
-    projectId: "malikkurosaki1985",
-    storageBucket: "malikkurosaki1985.appspot.com",
-    messagingSenderId: "27222609089",
-    appId: "1:27222609089:web:bf85a0777451fb17da9840"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAfACNHRoyIvX4nct4juVabZDgwEDKQ6jY",
+//     authDomain: "malikkurosaki1985.firebaseapp.com",
+//     databaseURL: "https://malikkurosaki1985.firebaseio.com",
+//     projectId: "malikkurosaki1985",
+//     storageBucket: "malikkurosaki1985.appspot.com",
+//     messagingSenderId: "27222609089",
+//     appId: "1:27222609089:web:bf85a0777451fb17da9840"
+// };
 
-initializeApp(firebaseConfig);
-const db = getDatabase();
+// initializeApp(firebaseConfig);
+// const db = getDatabase();
 
-const starCountRef = ref(db, 'gambar');
-onValue(starCountRef, (snapshot) => {
-    const data = snapshot.val();
-    gambar.html(`<img src="data:image/png;base64,${data}" />`);
-});
+// const starCountRef = ref(db, 'gambar');
+// onValue(starCountRef, (snapshot) => {
+//     const data = snapshot.val();
+//     //gambar.html(`<img src="data:image/png;base64,${data}" />`);
+// });
 
-const logRef = ref(db, 'console');
-onValue(logRef, (snapshot) => {
-    /**@type {String} */
-    const data = snapshot.val();
-    log.html(data.split("\n").join("</br>"));
-});
+// const logRef = ref(db, 'console');
+// onValue(logRef, (snapshot) => {
+//     /**@type {String} */
+//     const data = snapshot.val();
+//     log.html(data.split("\n").join("</br>"));
+// });
 
 
 
---
+++ x
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+login="sshpass -p gg123@#$ ssh root@47.250.56.127"
+gitAwal() {
+    echo "# ytb" >>README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/malikkurosaki/ytb.git
+    git push -u origin main
+}
+
+menu() {
+    local m="
+    YTB                             MENU
+
+
+    "
+
+    echo "$m"
+}
+
+npmInit() {
+    local n=$(npm i puppeteer express express-async-handler cors)
+    echo -e "$n"
+}
+
+push() {
+
+    git add .
+    git commit -m "update"
+    git push origin main -f
+    $login -t "cd /root/makuro/ytb && git pull origin main && source ~/.nvm/nvm.sh && npm install "
+
+}
+
+npm() {
+    case $2 in
+    init) npmInit ;;
+    *) echo "npm" ;;
+    esac
+}
+
+waktu() {
+
+    while true; do
+        wak=$(date +"%H:%M:%S")
+        clear
+        echo -e "waktunya adalah: $wak \n"
+        sleep 1
+    done
+}
+
+single() {
+    case $1 in
+    i) menu ;;
+    start) pm2 start controller/jalankan.js --name "ytb" ;;
+    log) pm2 logs --name "ytb" ;;
+    wk) waktu ;;
+    push) push ;;
+    *) echo "[command]" ;;
+    esac
+}
+
+double() {
+    case "$1$2" in
+    local-build) npm run build;;
+    local-start) nodemon controller/jalankan.js --ignore cookies.json & npm run dev ;;
+    server-login) $login ;;
+    server-ls) $login -t "cd /root/makuro/ytb && ls " ;;
+    server-run) $login -t "cd /root/makuro/ytb && source ~/.nvm/nvm.sh && pm2 start server.js --name server" ;;
+    server-start) $login -t "cd /root/makuro/ytb && source ~/.nvm/nvm.sh && pm2 start controller/jalankan.js --name ytb" ;;
+    server-status) $login -t "cd /root/makuro/ytb && source ~/.nvm/nvm.sh && pm2 status" ;;
+    server-restart) $login -t "cd /root/makuro/ytb && source ~/.nvm/nvm.sh && pm2 restart all " ;;
+    server-log) $login -t "cd /root/makuro/ytb && source ~/.nvm/nvm.sh && pm2 logs" ;;
+    server-stop) $login -t "cd /root/makuro/ytb && source ~/.nvm/nvm.sh && pm2 stop ytb" ;;
+    server-delete) $login -t "cd /root/makuro/ytb && source ~/.nvm/nvm.sh && pm2 delete ytb" ;;
+    *) echo "[command] [param]" ;;
+    esac
+}
+
+if [[ -z $1 && -z $2 ]]; then
+    menu
+elif [[ ! -z $1 && -z $2 ]]; then
+    single $1
+elif [[ ! -z $1 && ! -z $2 ]]; then
+    double $1 $2
+else
+    echo "error"
+fi
