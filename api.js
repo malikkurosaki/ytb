03708074d051c975281da0e657f15c4f7ee981a7
malikkurosaki <kurosakiblackangel@gmail.com>
2022-02-21 02:26:21 +0800

    update

--- api.js
+++ api.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const Api = express.Router();
 const path = require('path');
+const asyncHandler = require('express-async-handler');
+const shelljs= require('shelljs');
 
 Api.get('/youtube', (req, res) => { 
     res.sendFile(path.join(__dirname, './public/youtube.html'));
 });
+
+Api.get('/restart', asyncHandler(async (req, res) => {
+    let shl = shelljs.exec('pm2 restart all').stdout;
+    res.json(shl)
+}))
 
 module.exports = Api;
